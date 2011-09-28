cd6ca526052f408429a16f51660f8a43d1ea76a5
Endy Muhardin <endy.muhardin@gmail.com>
2011-09-27 23:08:09 -0700

    add webservice operation.
    - simpan
    - findSemua
    - countSemua

--- aplikasi-manajemen-peserta-jaxws/src/main/java/aplikasi/peserta/ManajemenPesertaWS.java
+++ aplikasi-manajemen-peserta-jaxws/src/main/java/aplikasi/peserta/ManajemenPesertaWS.java
@@ -7,6 +7,7 @@ package aplikasi.peserta;
 import aplikasi.peserta.domain.Peserta;
 import aplikasi.peserta.service.ManajemenPesertaService;
 import aplikasi.peserta.service.impl.dummy.ManajemenPesertaServiceDummy;
+import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -18,7 +19,7 @@ import javax.jws.WebParam;
 @WebService(serviceName = "ManajemenPesertaWS")
 public class ManajemenPesertaWS {
 
-    ManajemenPesertaService service = new ManajemenPesertaServiceDummy();
+    private static ManajemenPesertaService service = new ManajemenPesertaServiceDummy();
     
     
     
@@ -34,7 +35,35 @@ public class ManajemenPesertaWS {
     @WebMethod(operationName = "findPesertaByNomerPeserta")
     public Peserta findPesertaByNomerPeserta(@WebParam(name = "nomerPeserta")
     String nomerPeserta) {
-        //TODO write your implementation code here:
+        
         return service.findPesrtaByNomerPeserta(nomerPeserta);
     }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "simpan")
+    public Peserta simpan(@WebParam(name = "peserta")  Peserta peserta) {
+        service.simpan(peserta);
+        return peserta;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "countSemuaPeserta")
+    public Long countSemuaPeserta() {
+        return service.countSemuaPeserta();
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "findSemuaPeserta")
+    public List findSemuaPeserta(@WebParam(name = "start") Integer start, 
+    @WebParam(name = "rows") Integer rows) {
+        return service.findSemuaPeserta(start, rows);
+    }
+    
+    
 }
